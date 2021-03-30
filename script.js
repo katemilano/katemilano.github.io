77828455c48e121e0c58ba42a6a1f37a8013035e
Kate Milano <catherine.ann.milano@gmail.com>
2021-03-30 14:56:07 -0400

    added to portfolio

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ $("#b21").on("click", function(){
     $("#d1").css("height","10");
     $("#d1").css("transition","1s");
     $("#b1").css("opacity","1");
-    console.log("yes");
 });
 
 $("#b2").on("click", function(){
@@ -27,5 +26,19 @@ $("#b22").on("click", function(){
     $("#d2").css("height","10");
     $("#d2").css("transition","1s");
     $("#b2").css("opacity","1");
-    console.log("yes");
 });
+
+$("#b3").on("click", function(){
+    $("#d3").css("opacity","1");
+    $("#d3").css("height","520");
+    $("#d3").css("transition","1s");
+    $("#b3").css("opacity","0");
+});
+
+$("#b32").on("click", function(){
+    $("#d3").css("opacity","0");
+    $("#d3").css("height","10");
+    $("#d3").css("transition","1s");
+    $("#b3").css("opacity","1");
+});
+
