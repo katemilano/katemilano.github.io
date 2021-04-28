ea642c3bcef3c0946c196844c807007670d70e31
Kate Milano <catherine.ann.milano@gmail.com>
2021-04-28 12:33:19 -0400

    nav bar basics

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 import { Link as LinkRouter } from 'react-router-dom';
-// import { Link as LinkScroll } from 'react-scroll'
+import { Link as LinkScroll } from 'react-scroll'
 
 export const Nav = styled.nav`
-    background: #add9e;
+    background: #cef1f7;
+    height: 80px;
+    justify-content: center;
+    font-size: 1rem;
     display: flex;
+    position: sticky;
+    top: 0;
+    z-index: 10;
 
     @media screen and (max-width: 960px) {
         transition 0.8s all ease;
@@ -13,10 +19,16 @@ export const Nav = styled.nav`
 
 export const NavbarContainer = styled.div`
     display: flex;
+    justify-content: space-between;
+    height: 80px;
     z-index: 1;
+    width: 100%;
+    padding: 0 24px;
+    max-width: 1100px;
+    background-color: #add8e5;
 `;
 export const NavLogo = styled(LinkRouter)`
-    color: #ffff
+    color: white;
     justify-self: flex-start;
     cursor: pointer;
     font-size: 1.5rem;
@@ -25,4 +37,48 @@ export const NavLogo = styled(LinkRouter)`
     margin-left: 24px;
     font-weight: bold;
     text-decoration: none;
+`;
+
+export const MobileIcon = styled.div`
+    display: none;
+    color: white;
+
+    @media screen and (max-width: 768px) {
+        display:block;
+        position:absolute;
+        top: 0;
+        right: 0;
+        transform: translate (-100%, 60%);
+        font-size: 1.8rem;
+        cursor: pointer;
+    }
+`;
+
+export const NavMenu = styled.ul`
+    display: flex;
+    align-items: center;
+    list-style: none;
+    text-align: center;
+    
+    @media screen and (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export const NavItem = styled.li`
+    height: 80px;
+`;
+
+export const NavLinks = styled(LinkScroll)`
+    color: #fff;
+    display: flex;
+    align-item: center;
+    text-decoration: none;
+    padding: 0 1rem;
+    height: 100%;
+    cursor: pointer:
+
+    &.active{
+        border-bottom: 3px solid #01bf71;
+    }
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Nav, NavbarContainer, NavLogo} from './NavbarElements';
+import {FaBars} from 'react-icons/fa'
+import {
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
             <Nav>
                 <NavbarContainer>
-                    <NavLogo>Catherine Milano</NavLogo>
+                    <NavLogo to='/'>Catherine Milano</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='home'>Home</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='projects'>Projects</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='skills'>Skills</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='contact'>Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
                 </NavbarContainer>
             </Nav>
         </>
